61857c902b188e397001a407e9c801b3831323c5
Wizard Lee <282539259@qq.com>
2016-01-19 16:04:15 +0800

    finish custom transition

--- Animations/CustomShowViewController.swift
+++ Animations/CustomShowViewController.swift
@@ -9,7 +9,78 @@
 import UIKit
 
 class CustomShowViewController : UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationController?.delegate = self
+    }
+    
     @IBAction func quit(sender: UIBarButtonItem) {
-        self.navigationController?.dismissViewControllerAnimated(true, completion: nil)
+        navigationController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+}
+
+extension CustomShowViewController : UINavigationControllerDelegate {
+    
+    func navigationController(navigationController: UINavigationController, animationControllerForOperation operation: UINavigationControllerOperation, fromViewController fromVC: UIViewController, toViewController toVC: UIViewController) -> UIViewControllerAnimatedTransitioning? {
+        switch operation {
+        case .Push:
+            return FadeAndScaleTransitioningPush()
+        case .Pop:
+            return FadeAndScaleTransitioningPop()
+        case .None:
+            return nil
+        }
+    }
+    
+}
+
+class FadeAndScaleTransitioningPush : NSObject, UIViewControllerAnimatedTransitioning {
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 1
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+        
+        transitionContext.containerView()?.addSubview(toViewController.view)
+        toViewController.view.transform = CGAffineTransformMakeScale(0.1, 0.1)
+        toViewController.view.alpha = 0.0
+        
+        UIView.animateWithDuration(transitionDuration(transitionContext), animations: {
+            fromViewController.view.alpha = 0.0
+            toViewController.view.transform = CGAffineTransformIdentity
+            toViewController.view.alpha = 1.0
+            })
+            { _ in
+                fromViewController.view.alpha = 1.0
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+        }
+    }
+}
+
+class FadeAndScaleTransitioningPop : NSObject, UIViewControllerAnimatedTransitioning {
+    
+    func transitionDuration(transitionContext: UIViewControllerContextTransitioning?) -> NSTimeInterval {
+        return 1
+    }
+    
+    func animateTransition(transitionContext: UIViewControllerContextTransitioning) {
+        let toViewController = transitionContext.viewControllerForKey(UITransitionContextToViewControllerKey)!
+        let fromViewController = transitionContext.viewControllerForKey(UITransitionContextFromViewControllerKey)!
+        
+        transitionContext.containerView()?.addSubview(toViewController.view)
+        toViewController.view.alpha = 0
+        
+        UIView.animateWithDuration(transitionDuration(transitionContext),
+            animations: {
+                fromViewController.view.transform = CGAffineTransformMakeScale(0.1, 0.1)
+                toViewController.view.alpha = 1
+            })
+            { _ in
+                fromViewController.view.transform = CGAffineTransformIdentity
+                transitionContext.completeTransition(!transitionContext.transitionWasCancelled())
+        }
     }
 }
