09c624a125e6ba82e81b77230d9d0ce01981c72a
IamAlchemist <wizard.lee@qq.com>
2016-01-19 17:56:09 +0800

    minor change

---
+++ Animations/InteractiveShow.swift
@@ -0,0 +1,13 @@
+//
+//  InteractiveShow.swift
+//  Animations
+//
+//  Created by wizard on 1/19/16.
+//  Copyright © 2016 Alchemist. All rights reserved.
+//
+
+import UIKit
+
+class InteractiveShowViewController : UIViewController {
+    
+}
