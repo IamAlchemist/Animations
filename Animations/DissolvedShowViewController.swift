94f2b530ee523e874b26959f15ce63c09679a618
Wizard Lee <282539259@qq.com>
2016-01-21 11:19:48 +0800

    have a try filtered image view

--- Animations/DissolvedShowViewController.swift
+++ Animations/DissolvedShowViewController.swift
@@ -10,8 +10,34 @@ import UIKit
 
 class DissolvedShowViewController : UIViewController {
     
+    @IBOutlet weak var dissolvedImageView: FilteredImageView!
+    
     @IBAction func quit(sender: AnyObject) {
         navigationController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    var displayLink : CADisplayLink?
+    
+    var pixelScale : Float = 8
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dissolvedImageView.contentMode = .ScaleAspectFit
+        dissolvedImageView.inputImage = UIImage(named: "john-paulson")
+        dissolvedImageView.filter = CIFilter(name: "CIPixellate")
+        
+        displayLink = CADisplayLink(target: self, selector: "update:")
+        displayLink?.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
+        displayLink?.paused = true
+    }
+    
+    func update(displayLink : CADisplayLink) {
+        print("update : \(pixelScale)")
+        pixelScale += 2
+        pixelScale = pixelScale > 100 ? 2 : pixelScale;
+        let param = ScalarFilterParameter(key: "inputScale", value: pixelScale)
+        dissolvedImageView.parameterValueDidChange(param)
     }
 }
 
