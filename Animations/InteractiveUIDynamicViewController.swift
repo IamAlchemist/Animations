40dfed73ecc77bb47b4f069c9cbd94eab5d7543e
IamAlchemist <wizard.lee@qq.com>
2016-05-04 14:54:59 +0800

    add empty InteractiveDynamic

---
+++ Animations/InteractiveUIDynamicViewController.swift
@@ -0,0 +1,9 @@
+//
+//  InteractiveUIDynamic.swift
+//  DemoAnimations
+//
+//  Created by wizard on 5/4/16.
+//  Copyright © 2016 Alchemist. All rights reserved.
+//
+
+import Foundation
--- Animations/MenuViewController.swift
+++ Animations/MenuViewController.swift
@@ -93,7 +93,8 @@ class MenuDataSource : NSObject, UITableViewDataSource {
                   "ShowClockFace3",
                   "ShowGroupAnim",
                   "ShowBezierPath",
-                  "ShowCustomContainer"]
+                  "ShowCustomContainer",
+                  "ShowInteractiveUIDynamic"]
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("MenuCell", forIndexPath: indexPath) as! MenuTableViewCell
