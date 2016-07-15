bfe62f7658209df9bc419326a2f91b35ec932794
IamAlchemist <Wizard.Lee@qq.com>
2016-07-15 13:19:53 +0800

    minor change

--- Animations/TransitionDemoViewController.swift
+++ Animations/TransitionDemoViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class TransitionDemoViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var index : Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -18,6 +22,19 @@ class TransitionDemoViewController: UIViewController {
     
     func dismiss() {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    
+    @IBAction func switchImage(sender: UIButton) {
+        let images = [UIImage(named:"Anchor"), UIImage(named:"Cone"), UIImage(named:"IgIoo"), UIImage(named:"Spaceship")]
+        
+        UIView.transitionWithView(imageView, duration: 1.0,
+                                  options: UIViewAnimationOptions.TransitionCrossDissolve,
+                                  animations: {
+                                    self.imageView.image = images[self.index % images.count]
+                                    self.index += 1
+                                  },
+                                  completion: nil)
     }
 }
 
