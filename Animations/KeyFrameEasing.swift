7feee6f217d4689fc14bfe7782499fba6f96915c
IamAlchemist <Wizard.Lee@qq.com>
2016-07-17 16:01:23 +0800

    key frame easing

--- Animations/KeyFrameEasing.swift
+++ Animations/KeyFrameEasing.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class KeyFrameEasingViewController: UIViewController {
     @IBOutlet weak var container: UIView!
+    var ballView : UIImageView!
     
     var colorLayer : CALayer!
     
@@ -21,12 +22,11 @@ class KeyFrameEasingViewController: UIViewController {
         colorLayer.frame = container.bounds
         
         container.layer.addSublayer(colorLayer)
+        
+        let ballImage = UIImage(named: "Ball")
+        ballView = UIImageView(image: ballImage)
+        container.addSubview(ballView)
     }
-    
-//    override func viewDidAppear(animated: Bool) {
-//        super.viewDidAppear(animated)
-//        colorLayer.frame = container.bounds
-//    }
     
     @IBAction func changeColor(sender: UIButton) {
         colorLayer.removeAllAnimations()
@@ -42,4 +42,88 @@ class KeyFrameEasingViewController: UIViewController {
         
         colorLayer.addAnimation(animation, forKey: nil)
     }
+
+    @IBAction func bounce(sender: UIButton) {
+        animate()
+    }
+    
+    private func animate() {
+        ballView.center = CGPoint(x: 150, y: 32)
+        
+        let fromValue = NSValue(CGPoint: CGPoint(x: 150, y: 32))
+        let toValue = NSValue(CGPoint: CGPoint(x: 150, y: 268))
+        
+        let numFrames = 60
+        var frames = [NSValue]()
+        
+        for i in 0..<numFrames {
+            var time = 1.0/Float(numFrames) * Float(i)
+            time = bounceEaseOut(time)
+            frames.append(interpolateFromValue(fromValue, toValue: toValue, time: time))
+        }
+        
+        let keyframeAnimation = CAKeyframeAnimation()
+        keyframeAnimation.keyPath = "position"
+        keyframeAnimation.duration = 1.0
+        keyframeAnimation.values = frames
+        
+        ballView.layer.position = CGPoint(x: 150, y: 268)
+        ballView.layer.addAnimation(keyframeAnimation, forKey: nil)
+    }
+    
+    private func interpolateFromValue(fromValue: NSValue, toValue:NSValue, time:Float) -> NSValue {
+        let from = fromValue.CGPointValue()
+        let to = toValue.CGPointValue()
+        
+        let result = CGPoint(x: interpolate(from.x, to.x, time), y: interpolate(from.y, to.y, time))
+        return NSValue(CGPoint:result)
+    }
+    
+    private func interpolate(from: CGFloat, _ to: CGFloat, _ time: Float) -> CGFloat {
+        return (to - from) * CGFloat(time) + from;
+    }
+    
+    private func bounceEaseOut(t: Float) -> Float {
+        if (t < 4/11.0) {
+            return (121 * t * t)/16.0;
+        }
+        else if (t < 8/11.0) {
+            return (363/40.0 * t * t) - (99/10.0 * t) + 17/5.0;
+        }
+        else if (t < 9/10.0) {
+            return (4356/361.0 * t * t) - (35442/1805.0 * t) + 16061/1805.0;
+        }
+        
+        return (54/5.0 * t * t) - (513/25.0 * t) + 268/25.0;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
